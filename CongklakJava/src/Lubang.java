a12f3483133d93277164183da0feab3f1a56ab5b
StephanieTania <>
2016-10-21 19:40:55 +0700

    tambah kelas Lubang

---
+++ CongklakJava/src/Lubang.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author StephanieTania
+ */
+public class Lubang {
+    private int isi;
+    private boolean bisaDiisi;
+
+    public Lubang(int isi) {
+        this.isi = isi;
+        bisaDiisi=true;
+    }
+    
+}
