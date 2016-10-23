4d4b099e431a043381d2daf8beecb1ce04145a77
StephanieTania <>
2016-10-23 22:44:05 +0700

    perbaikan tester

--- CongklakJava/src/Tester.java
+++ CongklakJava/src/Tester.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,21 +18,28 @@ public class Tester {
      */
     public static void main(String[] args) {
         Game g=new Game();
+        Scanner sc=new Scanner(System.in);
         g.printPapan();
-        //while(!g.cekAkhirGame()){
+        while(!g.cekAkhirGame()){
             if(g.getGiliran()==0){
                 System.out.print("Giliran Player 1: (0,1,2,..,6)\n");
-                g.gerak(0);
+                int idx=sc.nextInt();
+                
+                //cek input, harus valid
+                
+                g.gerak(idx);
                 g.printPapan();
             }
             else{
                 System.out.println("Giliran Player 2: (7,8,9,..,13)\n");
-                g.gerak(0);
+                int idx=sc.nextInt();
+                
+                //cek input, harus valid
+                
+                g.gerak(idx);
                 g.printPapan();
             }
-        //}
-            g.gerak(1);
-            g.printPapan();
+        }            
     }
     
 }
